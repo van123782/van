aa2f6bee69b8ab4bcd2176181ec8b59ad5e2dfe2
van123782 <67407049+van123782@users.noreply.github.com>
2020-11-11 13:06:15 -0600

    Delete CtrlSaludo.js

--- js/CtrlSaludo.js
+++
@@ -1,9 +0,0 @@
-import { valida } from "../lib/util.js";
-export class CtrlSaludo {
-  /** @param {string} nombre */
-  calculaSaludo(nombre) {
-    const nombreSinEspacios = nombre ? nombre.trim() : "";
-    valida(nombreSinEspacios, "Falta proporcionar el nombre.");
-    return `Hola ${nombreSinEspacios}.`;
-  }
-}
