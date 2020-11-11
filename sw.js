807c7150523ceb023add885ceddce37bcc5b977e
van123782 <67407049+van123782@users.noreply.github.com>
2020-11-11 17:50:42 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ const CACHE = "van-1.00";
 /**  Archivos requeridos para que la aplicación funcione fuera de línea. */
 const ARCHIVOS = [
   "cmp/mi-footer.js",
-  "css/estilos.css",
+
   "img/icono256.png",
   "img/icono1024.png",
   "img/icono2048.png",
